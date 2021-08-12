242877d1875dca5499c26103eec4b2feb3fc8021
Ilkin <44848231+ilkinaslanli2001@users.noreply.github.com>
2021-08-12 18:38:37 +0400

    Update asgi.py

--- react_chat_backend/react_chat_backend/asgi.py
+++ react_chat_backend/react_chat_backend/asgi.py
@@ -1,35 +1,18 @@
-"""
-ASGI config for react_chat_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
 
 import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "react_chat_backend.settings")
+django.setup()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.contrib.auth.models import AnonymousUser
 from channels.routing import get_default_application
 import chat.routing
 from channels.db import database_sync_to_async
-import django
 from user.models import User
-import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "react_chat_backend.settings")
-django.setup()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": QueryAuthMiddleware(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
 
 @database_sync_to_async
 def get_user(user_id):
@@ -53,3 +36,12 @@ class QueryAuthMiddleware:
         return await self.app(scope, receive, send)
 
 
+
+application = ProtocolTypeRouter({
+    "http": get_default_application()
+    "websocket": QueryAuthMiddleware(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
